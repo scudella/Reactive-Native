ac81f908183cd0d50e0a38aa6feb7af70e34aed5
Eduardo S. Libardi <eslibardi@gmail.com>
2023-09-11 09:21:28 -0300

    navigation props and useNavigation

---
+++ 06-Navigation/screens/CategoriesScreen.js
@@ -0,0 +1,29 @@
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate('MealsOverview');
+    };
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+export default CategoriesScreen;
